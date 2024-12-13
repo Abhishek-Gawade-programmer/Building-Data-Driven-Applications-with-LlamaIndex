144a094127b2b0372d93d197304df49700af156f
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-13 19:00:28 +0530

    Refactor sample_Create_Node.py: updated document creation and node instantiation. Replaced hardcoded text with a new example string and improved variable naming for clarity.

--- ch3/sample_Create_Node.py
+++ ch3/sample_Create_Node.py
@@ -1,8 +1,10 @@
-from llama_index.core import Document
 from llama_index.core.schema import TextNode
+from llama_index.core import Document
 
-doc = Document(text="This is a sample document text")
-n1 = TextNode(text=doc.text[0:16], doc_id=doc.id_) 
-n2 = TextNode(text=doc.text[17:30], doc_id=doc.id_)
-print(n1)
-print(n2)
+text = "The quick brown fox jumps over the lazy dog."
+doc = Document(text=text)
+node1 = TextNode(text=doc.text[0:16], doc_id=doc.id_)
+node2 = TextNode(text=doc.text[17:30], doc_id=doc.id_)
+
+print(node1)
+print(node2)
