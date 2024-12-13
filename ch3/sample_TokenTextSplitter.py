5a863d527c3f129cc248be2257d4640c39491fbb
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-13 21:31:59 +0530

    Refactor sample_TokenTextSplitter.py: streamline document creation and enhance node processing. Updated to use show_progress parameter in get_nodes_from_documents for improved user feedback.

--- ch3/sample_TokenTextSplitter.py
+++ ch3/sample_TokenTextSplitter.py
@@ -1,20 +1,16 @@
-from llama_index.core import Document
 from llama_index.core.node_parser import TokenTextSplitter
+from llama_index.core.schema import Document
 
-doc = Document( 
-    text=(
-    "This is sentence 1. This is sentence 2. "
-    "Sentence 3 here."
-    ),
-    metadata={"author": "John Smith"}
-)  
-splitter = TokenTextSplitter( 
-    chunk_size=12, 
-    chunk_overlap=0, 
-    separator=" "
-) 
-
-nodes = splitter.get_nodes_from_documents([doc]) 
-for node in nodes: 
-    print(node.text) 
+doc = Document(
+    text=("This is sentence 1. This is sentence 2. " "Sentence 3 here."),
+    metadata={"author": "John Smith"},
+)
+splitter = TokenTextSplitter(
+    chunk_size=12,
+    chunk_overlap=0,
+    separator=" ",
+)
+nodes = splitter.get_nodes_from_documents([doc], show_progress=True)
+for node in nodes:
+    print(node.text)
     print(node.metadata)
