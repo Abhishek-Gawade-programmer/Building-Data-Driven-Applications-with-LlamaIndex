7f78848344aa6649bea56244e9658187b260f829
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-13 18:57:14 +0530

    Refactor code for consistency: updated string formatting in sample1.py, sample_Create_Doc_from_Wiki.py, and sample_Create_Doc.py to use double quotes. Cleaned up whitespace and removed unnecessary line breaks for improved readability.

--- ch3/sample_Create_Doc_from_Wiki.py
+++ ch3/sample_Create_Doc_from_Wiki.py
@@ -1,7 +1,5 @@
 from llama_index.readers.wikipedia import WikipediaReader
 
-loader = WikipediaReader() 
-documents = loader.load_data(
-    pages=['Pythagorean theorem','General relativity']
-) 
+loader = WikipediaReader()
+documents = loader.load_data(pages=["Pythagorean theorem", "General relativity"])
 print(f"loaded {len(documents)} documents")
