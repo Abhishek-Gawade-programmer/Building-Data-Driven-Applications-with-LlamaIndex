ab9c07321f44ea051cb6d20712c87ffb2bebccdf
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-16 14:16:15 +0530

    KeywordTableIndex

--- ch5/sample_KeywordTableIndex.py
+++ ch5/sample_KeywordTableIndex.py
@@ -1,6 +1,8 @@
 from llama_index.core import KeywordTableIndex, SimpleDirectoryReader
-documents = SimpleDirectoryReader("files").load_data()
+import rich
+
+documents = SimpleDirectoryReader("files").load_data(show_progress=True)
 index = KeywordTableIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 response = query_engine.query("What famous buildings were in ancient Rome?")
-print(response)
+rich.print(response)
