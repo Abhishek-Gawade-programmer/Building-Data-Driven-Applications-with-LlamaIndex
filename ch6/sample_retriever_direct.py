9ad2ca242683ec3b1e726576471898cb74286568
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-16 18:47:31 +0530

    rerivale

--- ch6/sample_retriever_direct.py
+++ ch6/sample_retriever_direct.py
@@ -3,8 +3,6 @@ from llama_index.core.retrievers import SummaryIndexEmbeddingRetriever
 
 documents = SimpleDirectoryReader("files").load_data()
 summary_index = SummaryIndex.from_documents(documents)
-retriever = SummaryIndexEmbeddingRetriever(
-    index=summary_index
-)
+retriever = SummaryIndexEmbeddingRetriever(index=summary_index)
 result = retriever.retrieve("Tell me about ancient Rome")
 print(result[0].text)
--- ch6/sample_retriever_from_index1.py
+++ ch6/sample_retriever_from_index1.py
@@ -1,9 +1,11 @@
 from llama_index.core import SummaryIndex, SimpleDirectoryReader
+import rich
 
 documents = SimpleDirectoryReader("files").load_data()
+
 summary_index = SummaryIndex.from_documents(documents)
-retriever = summary_index.as_retriever(
-    retriever_mode='embedding'
-)
+
+retriever = summary_index.as_retriever(retriever_mode="embedding")
+
 result = retriever.retrieve("Tell me about ancient Rome")
-print(result[0].text)
+rich.print(result)
