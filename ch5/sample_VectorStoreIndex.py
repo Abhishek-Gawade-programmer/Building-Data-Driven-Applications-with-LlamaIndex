90bb6ca73438cb4564ec4978c5b547e5b6455581
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-16 10:25:24 +0530

    some indexs things

--- ch5/sample_VectorStoreIndex.py
+++ ch5/sample_VectorStoreIndex.py
@@ -1,4 +1,8 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import rich
+
 documents = SimpleDirectoryReader("files").load_data()
+
 index = VectorStoreIndex.from_documents(documents)
+rich.print(index)
 print("Index created successfully!")
--- ch5/sample_local_embedding.py
+++ ch5/sample_local_embedding.py
@@ -1,9 +1,7 @@
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-embedding_model = HuggingFaceEmbedding(
-    model_name="WhereIsAI/UAE-Large-V1"
-)
+
+embedding_model = HuggingFaceEmbedding(model_name="WhereIsAI/UAE-Large-V1")
 embeddings = embedding_model.get_text_embedding(
     "The quick brown fox jumps over the lazy cat!"
 )
-print(embeddings[:15]) 
-
+print(embeddings[:15])
