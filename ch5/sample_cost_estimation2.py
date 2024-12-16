372f10f80f25abc71a842de0c780c2137deff2e7
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-16 16:13:48 +0530

    cost

--- ch5/sample_cost_estimation2.py
+++ ch5/sample_cost_estimation2.py
@@ -1,5 +1,10 @@
 import tiktoken
-from llama_index.core import MockEmbedding, VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core import (
+    MockEmbedding,
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    Settings,
+)
 from llama_index.core.callbacks import CallbackManager, TokenCountingHandler
 from llama_index.core.llms.mock import MockLLM
 
@@ -9,19 +14,17 @@ token_counter = TokenCountingHandler(
     tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode
 )
 callback_manager = CallbackManager([token_counter])
-
-Settings.embed_model=embed_model
-Settings.llm=llm
-Settings.callback_manager=callback_manager
+Settings.embed_model = embed_model
+Settings.llm = llm
+Settings.callback_manager = callback_manager
 
 documents = SimpleDirectoryReader("cost_prediction_samples").load_data()
-index = VectorStoreIndex.from_documents(
-    documents=documents,
-    show_progress=True)
+index = VectorStoreIndex.from_documents(documents=documents, show_progress=True)
+
 print("Embedding Token Count:", token_counter.total_embedding_token_count)
 
-query_engine = index.as_query_engine()  
-response = query_engine.query("What's the cat's name?")   
+query_engine = index.as_query_engine()
+response = query_engine.query("What's the cat's name?")
+print(response)
 print("Query LLM Token Count:", token_counter.total_llm_token_count)
-print("Query Embedding Token Count:",token_counter.total_embedding_token_count)
-
+print("Query Embedding Token Count:", token_counter.total_embedding_token_count)
