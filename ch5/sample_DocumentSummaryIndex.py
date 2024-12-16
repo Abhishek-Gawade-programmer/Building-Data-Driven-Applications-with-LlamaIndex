0aa6be4e83b31b552b4068f6a0a97888a911ce18
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-16 14:06:42 +0530

    document summary

--- ch5/sample_DocumentSummaryIndex.py
+++ ch5/sample_DocumentSummaryIndex.py
@@ -1,12 +1,12 @@
 from llama_index.core import DocumentSummaryIndex, SimpleDirectoryReader
 
 documents = SimpleDirectoryReader("files").load_data()
-index = DocumentSummaryIndex.from_documents(
-    documents, 
-    show_progress=True
-)
+
+index = DocumentSummaryIndex.from_documents(documents, show_progress=True)
+
 
 summary1 = index.get_document_summary(documents[0].doc_id)
 summary2 = index.get_document_summary(documents[1].doc_id)
-print("\nSummary of the first document:" + summary1)
-print("\nSummary of the second document:" + summary2)
+
+print("\n Summary of the first document: " + summary1)
+print("\n Summary of the second document: " + summary2)
