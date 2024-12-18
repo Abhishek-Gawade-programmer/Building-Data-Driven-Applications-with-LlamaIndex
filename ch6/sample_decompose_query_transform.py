d333fadd2f4b492c371a1e36d3b68fea5c2fd310
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-18 12:35:03 +0530

    DecomposeQueryTransform

--- ch6/sample_decompose_query_transform.py
+++ ch6/sample_decompose_query_transform.py
@@ -1,6 +1,11 @@
 from llama_index.core.indices.query.query_transform.base import DecomposeQueryTransform
+
+
 decompose = DecomposeQueryTransform()
+
 query_bundle = decompose.run(
-    "Tell me about buildings in ancient Rome"
+    "how men get hard fealling about the sex while materbuabating "
 )
+
+
 print(query_bundle.query_str)
