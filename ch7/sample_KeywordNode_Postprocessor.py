7b7ef066fdd15bc059fcb21ddbba9a6b5f2910cd
Abhishek Gawade <supeduperr.oa@gmail.com>
2024-12-19 23:00:56 +0530

    KeywordNodePostprocessor

--- ch7/sample_KeywordNode_Postprocessor.py
+++ ch7/sample_KeywordNode_Postprocessor.py
@@ -1,17 +1,25 @@
 from llama_index.core.postprocessor import KeywordNodePostprocessor
 from llama_index.core.schema import TextNode, NodeWithScore
+import rich
 
 nodes = [
-    TextNode(text="Entry no: 1, <SECRET> - Attack at Dawn"),
-    TextNode(text="Entry no: 2, <RESTRICTED> - Go to point Bravo"),
-    TextNode(text="Entry no: 3, <PUBLIC> - Roses are Red"),
+    TextNode(text="Entry no: 1, <SECRET>, Attack at Dawn"),
+    TextNode(text="Entry no: 2, <RESTRICTED>, Go to point Bravo"),
+    TextNode(text="Entry no: 3, <PUBLIC>, text: Roses are Red"),
+    TextNode(text="Entry no: 3, <PUBLIC>, text: i want $50k"),
 ]
 
 
 node_with_score_list = [NodeWithScore(node=node) for node in nodes]
+
+# rich.print(node_with_score_list)
+
 pp = KeywordNodePostprocessor(exclude_keywords=["SECRET", "RESTRICTED"])
+
 remaining_nodes = pp.postprocess_nodes(node_with_score_list)
+
 print("Remaining nodes:")
+
 for node_with_score in remaining_nodes:
     node = node_with_score.node
-    print(f"Text: {node.text}")
+    rich.print(f"Text: {node.text}")
